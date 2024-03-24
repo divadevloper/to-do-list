37f01650e67c84d9d7cc3733bf5f2a21bb812bd1
Danicode <ariyoesther2009@gmail.com>
2024-03-24 12:59:31 +0100

    Added something

--- function.js
+++ function.js
@@ -2,18 +2,6 @@ let input = document.getElementById('input')
 let tododiv = document.getElementById('tododiv')
 let todoarr = []
 
-// function addtodo() {
-// todoarr.unshift(input.value)
-// console.log(todoarr);
-//      tododiv.innerHTML =''
-// for (let index = 0; index < todoarr.length; index++) {
-// if (input.value=="") {
-//     alert('Enter to keep track for your daily activities')
-// }else{
-//     tododiv.innerHTML += `<p id="ptag"> ${todoarr[index]}  <button id="btn1"> <i class="fa-solid fa-trash-can"></i> </button> </p>`
-// }
-// }
-// }
 
 function addtodo(){
     if (input.value == '') {
@@ -29,7 +17,7 @@ function todelete(index) {
 
 }
 function toedit(index) {
-    let changimg = prompt("Enter the new value")
+    let changimg = prompt("Enter the new list")
     todoarr.splice(index,1, changimg);
     console.log(todoarr);
     disarr()
@@ -40,7 +28,7 @@ function disarr() {
     tododiv.innerHTML =''
     for (let index = 0; index < todoarr.length; index++){
         tododiv.innerHTML += `<p id="ptag"> ${todoarr[index]} 
-         <button onclick="todelete(${index})" id="btn1"> <i class="fa-solid fa-trash-can"></i> </button> 
+        <button onclick="todelete(${index})" id="btn1"> <i class="fa-solid fa-trash-can"></i> </button> 
          <button onclick="toedit(${index})" id="btn3"> edit </button>
         </p>`
      }
